c0af3079e26cb218f0d76c35121fa91d6cdfbf53
dhanushadimple <113236628+dhanushadimple@users.noreply.github.com>
2024-04-13 08:28:27 +0530

    Add files via upload

---
+++ Find.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Find
+{ 
+  public static void main(String args[])
+  {
+    int x;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the value");
+     x=sc.nextInt();
+     if(x%2==0)
+     {
+      System.out.println("Even no");
+     }
+     else
+     {
+      System.out.println("Odd no");
+     }
+  }
+}
